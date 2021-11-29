75b981c978bbf80f8daafe624b2928ab34fa84a2
bvoelker <bvoelker@fluidpay.com>
2021-11-29 13:13:47 -0600

    types - added some new error types

--- types.go
+++ types.go
@@ -13,9 +13,13 @@ var (
 	ErrValidation = errors.New("validation error")
 
 	// Permissions
-	ErrForbidden    = errors.New("forbidden error")
-	ErrPermission   = errors.New("permission error")
-	ErrUnauthorized = errors.New("unauthorized error")
+	ErrForbidden        = errors.New("forbidden error")
+	ErrPermission       = errors.New("permission error")
+	ErrUnauthorized     = errors.New("unauthorized error")
+	ErrMethodNotAllowed = errors.New("method not allowed error")
+	ErrNotAcceptable    = errors.New("not acceptable error")
+	ErrRequestTimeout   = errors.New("request timeout error")
+	ErrTooManyRequests  = errors.New("too many requests error")
 
 	// Database
 	ErrDBConnection = errors.New("database connection error")
